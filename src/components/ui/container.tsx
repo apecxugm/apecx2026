4422de9320d9bc5bef21db6542724c5495ed01f4
ayeah <187853299+ayashar@users.noreply.github.com>
2026-03-03 23:36:41 +0700

    init(layout): add container

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/src/lib/utils";
+
+const Container = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
+  return (
+    <section
+      className={cn(
+        `mx-auto flex w-full max-w-7xl flex-col gap-4 overflow-hidden px-4 md:px-10 xl:px-12`,
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default Container;
