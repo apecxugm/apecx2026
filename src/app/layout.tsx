11bcdc424cb6f3b2cebfc5c699320ae9ed6a8a66
ayeah <187853299+ayashar@users.noreply.github.com>
2026-03-03 23:34:58 +0700

    init(layout): add design system and button ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+}) 
+
+export const metadata: Metadata = {
+  title: "APECX 2026",
+  description: "Annual Petroleum Competition and Exhibition 2026 by SPE UGM SC",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${plusJakartaSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,41 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/src/lib/utils';
+
+const buttonVariants = cva(
+  'flex items-center justify-center rounded-lg md:text-base text-sm px-6 py-[10px] transition-transform duration-200 ease-in-out ',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary-700 text-white hover:bg-primary-900 has-[>svg]:gap-2 cursor-pointer hover:scale-105 font-bold',
+        light: 'bg-secondary-200 text-neutral-1000 hover:bg-secondary-400 has-[>svg]:gap-2 cursor-pointer hover:scale-105',
+        white: 'bg-neutral-100 font-bold text-neutral-1000 hover:bg-neutral-300 has-[>svg]:gap-2 cursor-pointer hover:scale-105',
+        'dark-blue': 'bg-tertiary-900 text-gray-700 text-white cursor-pointer hover:bg-tertiary-700 has-[>svg]:gap-2 hover:scale-105',
+      },
+      size: {
+        default: 'md:w-53 w-full',
+        fit: 'w-fit',
+        icon: 'p-[10px]',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  VariantProps<typeof buttonVariants> {
+  children?: React.ReactNode;
+}
+
+function Button({ className, variant, size, children, ...props }: ButtonProps) {
+  return (
+    <button className={cn(buttonVariants({ variant, size }), className)} {...props}>
+      {children}
+    </button>
+  );
+}
+
+export { Button, buttonVariants };
