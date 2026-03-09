fd991c8c03c820c9e9a2678303c782ed812eedad
rafaelaanabelpurba <rafaelaanabelpurba@mail.ugm.ac.id>
2026-03-09 15:36:43 +0700

    Add footer layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../modules/landing-page/hero";
+import Footer from "../modules/landing-page/footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Footer />
     </>
   );
 }
